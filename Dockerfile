fa4925e1646928c0a0ae1be083756fcf02de2428
Tom Erik Støwer <testower@gmail.com>
2018-03-24 20:15:53 +0100

    Use eslint recommended, allow browser and node envs

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /var/lib/eslint-linter
 
 RUN yarn global add eslint
 
-COPY "eslint-config-example.json" .
+COPY "eslint-config.json" .
 COPY "entrypoint.sh" .
 
 ENTRYPOINT [ "./entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,9 @@ COMMIT=$2
 git clone -q $REPO_URL repo
 git --git-dir=repo/.git --work-tree=repo checkout -q $COMMIT
 
-eslint --c eslint-config-example.json \
+eslint --c eslint-config.json \
    --no-eslintrc \
    --no-inline-config \
    --format json \
-   --env es6 \
-   --parser-options ecmaVersion:6,sourceType:module \
+   --parser-options ecmaVersion:9,sourceType:module \
    repo/**/*
